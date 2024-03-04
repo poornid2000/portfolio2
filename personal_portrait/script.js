df3ccec26337b927c57a5fb4dc68395647d5ca3a
Poornima Dissanayake <136261708+poornid2000@users.noreply.github.com>
2024-03-04 21:43:40 +0530

    Add files via upload

---
+++ personal_portrait/script.js
@@ -0,0 +1,36 @@
+
+
+    var tablinks = document.getElementsByClassName("tab-links");
+    var tabcontents = document.getElementsByClassName("tab-contents");
+
+    function opentab(tabname){
+        for(tablink of tablinks){
+            tablink.classList.remove("active-link");
+        }
+        for(tabcontent of tabcontents){
+            tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+    }
+
+
+
+
+    const scriptURL = '<https://script.google.com/macros/s/AKfycbzM3tRXFdd9neSCn3GCwvlrLlZ89MsxfD1pZ12TjUFWAsKGtDqsBenL9zq-51aaEQgP/exec>'
+    const form = document.forms['submit-to-google-sheet']
+    const msg=document.getElementById("msg")
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
+
